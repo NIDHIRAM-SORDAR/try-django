78fc34eb346f3b367c2c0b0438f002974169d069
NIDHIRAM-SORDAR <alsaeedctg@gmail.com>
2022-02-06 21:03:29 +0600

    changed allowed host

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'https://limitless-woodland-87233.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
